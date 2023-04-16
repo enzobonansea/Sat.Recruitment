c84bd384777d776b8d3d98dd5cfcb13f3043c897
enzo <enzo.bonansea@gmail.com>
2023-04-16 17:54:28 -0300

    test: create user use case

---
+++ Sat.Recruitment.Api/Domain/UserDuplicatedException.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Sat.Recruitment.Api.Domain
+{
+    public class UserDuplicatedException : Exception
+    {
+    }
+}
---
+++ Sat.Recruitment.Test/Application/CreateUserUseCaseTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Dynamic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Sat.Recruitment.Api.Domain;
+using Sat.Recruitment.Api.Application;
+using Sat.Recruitment.Api.Controllers;
+
+using Xunit;
+
+
+namespace Sat.Recruitment.Test.Application
+{
+    [CollectionDefinition("Tests", DisableParallelization = true)]
+    public class CreateUserUseCaseTest
+    {
+        [Fact]
+        public async Task ItShouldCreateUserIfNotExists()
+        {
+            var useCase = new CreateUserUseCase(new UserNotExistsRepository());
+            var exception = await Record.ExceptionAsync(() => useCase.Execute(Utils.RandomUser()));
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public async Task ItShouldNotCreateUserIfExists()
+        {
+            var useCase = new CreateUserUseCase(new UserExistsRepository());
+            var exception = await Record.ExceptionAsync(() => useCase.Execute(Utils.RandomUser()));
+            Assert.IsType<UserDuplicatedException>(exception);
+        }
+    }
+
+    class UserExistsRepository : IUsersRepository 
+    {
+        public Task<bool> Exists(User user) 
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task Save(User user) 
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    class UserNotExistsRepository : IUsersRepository 
+    {
+        public Task<bool> Exists(User user) 
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task Save(User user) 
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
