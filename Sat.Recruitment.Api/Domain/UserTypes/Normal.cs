5f4cd37e3a0a921559e23f98939ea63f7c60965a
enzo <enzo.bonansea@gmail.com>
2023-04-17 13:52:17 -0300

    feat(NormalUserType): get money

--- Sat.Recruitment.Api/Domain/UserTypes/Normal.cs
+++ Sat.Recruitment.Api/Domain/UserTypes/Normal.cs
@@ -6,7 +6,18 @@ namespace Sat.Recruitment.Api.Domain.UserTypes
     {
         public override decimal GetMoney(decimal baseMoney)
         {
-            return baseMoney;
+            if (baseMoney > 100)
+            {
+                return baseMoney + (baseMoney * Convert.ToDecimal(0.12));
+            }
+            else if (baseMoney < 100 && baseMoney > 10)
+            {
+                return baseMoney + (baseMoney * Convert.ToDecimal(0.08));
+            } 
+            else 
+            {
+                return baseMoney;
+            }
         }
     }
 }
