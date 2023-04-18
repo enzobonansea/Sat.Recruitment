f048c9993343bb2454c770258ef6baa2df0d8564
enzo <enzo.bonansea@gmail.com>
2023-04-17 21:53:07 -0300

    tests for legacy user controller

--- Sat.Recruitment.Test/Controllers/LegacyUsersControllerTest.cs
+++ Sat.Recruitment.Test/Controllers/LegacyUsersControllerTest.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Dynamic;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.FeatureManagement;
+
 
 using Sat.Recruitment.Api.Domain;
 using Sat.Recruitment.Api.Application;
@@ -15,15 +18,32 @@ using Xunit;
 
 namespace Sat.Recruitment.Test.Controllers
 {
-    [CollectionDefinition("Tests", DisableParallelization = true)]
-    public class UsersControllerTest
+    class LegacyBehaviorFeatureManager : IFeatureManager
     {
-        private readonly UsersController userController = new UsersController(new CreateUserUseCase(new FileUsersRepository(), NullLogger<CreateUserUseCase>.Instance), new CreateUserRequestValidator());
+        public IAsyncEnumerable<string> GetFeatureNamesAsync()
+        {
+            return null;
+        }
+
+        public Task<bool> IsEnabledAsync(string feature)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> IsEnabledAsync<TContext>(string feature, TContext context)
+        {
+            return Task.FromResult(false);
+        }
+    }
+    [CollectionDefinition("Tests", DisableParallelization = true)]
+    public class LegacyUsersControllerTest
+    {
+        private readonly UsersController userController = new UsersController(new CreateUserUseCase(new FileUsersRepository(), NullLogger<CreateUserUseCase>.Instance), new CreateUserRequestValidator(), new LegacyBehaviorFeatureManager());
 
         [Fact]
         public async Task ItMustCreateANewUser()
         {
-            var result = await userController.CreateUserAsync("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124");
+            var result = (Result)await userController.CreateUserAsync("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124");
 
             Assert.Equal(true, result.IsSuccess);
             Assert.Equal("User Created", result.Errors);
@@ -32,7 +52,7 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task ItMustNotCreateAnExistingUser()
         {
-            var result = await userController.CreateUserAsync("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124");
+            var result = (Result)await userController.CreateUserAsync("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124");
 
             Assert.Equal(false, result.IsSuccess);
             Assert.Equal("The user is duplicated", result.Errors);
@@ -41,7 +61,7 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task ItMustNotCreateAUserWithoutName()
         {
-            var result = await userController.CreateUserAsync(null, Utils.RandomMail(), Utils.RandomString(7), Utils.RandomString(7), "Normal", "124");
+            var result = (Result)await userController.CreateUserAsync(null, Utils.RandomMail(), Utils.RandomString(7), Utils.RandomString(7), "Normal", "124");
 
             Assert.Equal(false, result.IsSuccess);
             Assert.Equal(true, result.Errors.Contains(CreateUserRequestValidator.NameIsRequiredErrorMessage));
@@ -50,7 +70,7 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task ItMustNotCreateAUserWithoutEmail()
         {
-            var result = await userController.CreateUserAsync(Utils.RandomString(7), null, Utils.RandomString(7), Utils.RandomString(7), "Normal", "124");
+            var result = (Result)await userController.CreateUserAsync(Utils.RandomString(7), null, Utils.RandomString(7), Utils.RandomString(7), "Normal", "124");
 
             Assert.Equal(false, result.IsSuccess);
             Assert.Equal(true, result.Errors.Contains(CreateUserRequestValidator.EmailIsRequiredErrorMessage));
@@ -59,7 +79,7 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task ItMustNotCreateAUserWithoutAddress()
         {
-            var result = await userController.CreateUserAsync(Utils.RandomString(7), Utils.RandomMail(), null, Utils.RandomString(7), "Normal", "124");
+            var result = (Result)await userController.CreateUserAsync(Utils.RandomString(7), Utils.RandomMail(), null, Utils.RandomString(7), "Normal", "124");
 
             Assert.Equal(false, result.IsSuccess);
             Assert.Equal(true, result.Errors.Contains(CreateUserRequestValidator.AddressIsRequiredErrorMessage));
@@ -68,7 +88,7 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task ItMustNotCreateAUserWithoutPhone()
         {
-            var result = await userController.CreateUserAsync(Utils.RandomString(7), Utils.RandomMail(), Utils.RandomString(7), null, "Normal", "124");
+            var result = (Result)await userController.CreateUserAsync(Utils.RandomString(7), Utils.RandomMail(), Utils.RandomString(7), null, "Normal", "124");
 
             Assert.Equal(false, result.IsSuccess);
             Assert.Equal(true, result.Errors.Contains(CreateUserRequestValidator.PhoneIsRequiredErrorMessage));
