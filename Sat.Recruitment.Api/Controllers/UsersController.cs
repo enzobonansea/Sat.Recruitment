8061e03fd86df7d9f7ca51dece297e374a2270e1
enzo <enzo.bonansea@gmail.com>
2023-04-17 21:46:40 -0300

    using ComplyWithRest feature flag. Only off is developed

--- Sat.Recruitment.Api/Controllers/UsersController.cs
+++ Sat.Recruitment.Api/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
+using Sat.Recruitment.Api;
 using Sat.Recruitment.Api.Domain;
 using Sat.Recruitment.Api.Application;
 
@@ -23,16 +25,18 @@ namespace Sat.Recruitment.Api.Controllers
     {
         private readonly CreateUserRequestValidator createUserRequestValidator;
         private readonly CreateUserUseCase createUserUseCase;
+        private readonly IFeatureManager featureManager;
 
-        public UsersController(CreateUserUseCase createUserUseCase, CreateUserRequestValidator createUserRequestValidator)
+        public UsersController(CreateUserUseCase createUserUseCase, CreateUserRequestValidator createUserRequestValidator, IFeatureManager featureManager)
         {
             this.createUserUseCase = createUserUseCase;
             this.createUserRequestValidator = createUserRequestValidator;
+            this.featureManager = featureManager;
         }
 
         [HttpPost]
         [Route("/create-user")]
-        public async Task<Result> CreateUserAsync(string name, string email, string address, string phone, string userType, string money)
+        public async Task<object> CreateUserAsync(string name, string email, string address, string phone, string userType, string money)
         {
             try
             {
@@ -40,26 +44,48 @@ namespace Sat.Recruitment.Api.Controllers
 
                 await this.createUserUseCase.ExecuteAsync(new User(name, email, address, phone, userType, decimal.Parse(money)));
 
+                return await this.UserCreated();
+            }
+            catch (UserValidationException exception)
+            {
+                return await this.UserNotCreated(string.Join(" ", exception.Errors));
+            }
+            catch (UserDuplicatedException)
+            {
+                return await this.UserNotCreated("The user is duplicated");
+            }
+        }
+
+        private async Task<object> UserCreated()
+        {
+            if (await featureManager.IsEnabledAsync(FeatureFlags.ComplyWithRest))
+            {
+                // Return 201 Created
+                throw new NotImplementedException();
+            }
+            else
+            {
                 return new Result()
                 {
                     IsSuccess = true,
                     Errors = "User Created"
                 };
             }
-            catch (UserValidationException exception)
+        }
+
+        private async Task<object> UserNotCreated(string errors)
+        {
+            if (await featureManager.IsEnabledAsync(FeatureFlags.ComplyWithRest))
             {
-                return new Result()
-                {
-                    IsSuccess = false,
-                    Errors = string.Join(" ", exception.Errors)
-                };
+                // Return 400 BadRequest
+                throw new NotImplementedException();
             }
-            catch (UserDuplicatedException)
+            else
             {
                 return new Result()
                 {
                     IsSuccess = false,
-                    Errors = "The user is duplicated"
+                    Errors = errors
                 };
             }
         }
